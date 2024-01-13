872d434520a1938da43b2d7f6e67c4d8934f3686
Lefan Hu <lefanhu1@gmail.com>
2024-01-13 10:51:51 -0500

    added test page at /test for component display testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestPageComponent } from './pages/test-page/test-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: HomeComponent }, // sample format
+  { path: 'test', component: TestPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavSideBarComponent } from './components/nav-side-bar/nav-side-bar.component';
+import { TestPageComponent } from './pages/test-page/test-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavSideBarComponent,
+    TestPageComponent
   ],
   imports: [
     BrowserModule,
