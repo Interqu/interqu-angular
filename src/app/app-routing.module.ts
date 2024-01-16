01b0638e89c6c514ec81be1ee4cd6427f19ffcae
Anthero1 <anthonyeroukov@gmail.com>
2024-01-15 19:35:47 -0500

    Initial setup interview results page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TestPageComponent } from './pages/test-page/test-page.component';
+import { InterviewResultsComponent } from './pages/interview-results/interview-results.component';
+import { InterviewListComponent } from './components/interview-list/interview-list.component';
 
 const routes: Routes = [
   // { path: '', component: HomeComponent }, // sample format
-  { path: 'test', component: TestPageComponent }
+  { path: 'test', component: TestPageComponent },
+  { path: 'interview-results', component: InterviewResultsComponent },
+  { path: 'interview-list', component: InterviewListComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavSideBarComponent } from './components/nav-side-bar/nav-side-bar.component';
 import { TestPageComponent } from './pages/test-page/test-page.component';
+import { InterviewResultsComponent } from './pages/interview-results/interview-results.component';
+import { InterviewListComponent } from './components/interview-list/interview-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavSideBarComponent,
-    TestPageComponent
+    TestPageComponent,
+    InterviewResultsComponent,
+    InterviewListComponent
   ],
   imports: [
     BrowserModule,
