5bfc4867136df99f43ff6a5ccc111cf8f319661b
Lefan Hu <lefanhu1@gmail.com>
2024-01-21 22:40:34 -0500

    dashboard initial, commit transfer from main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TestPageComponent } from './pages/test-page/test-page.component';
 import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 
 const routes: Routes = [
-  // { path: '', component: HomeComponent }, // sample format
+  // { path: '', component: TestPageComponent }, // sample format
   { path: 'test', component: TestPageComponent },
-  { path: 'settings', component: SettingsPageComponent }
+  {
+    path: 'user',
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardPageComponent,
+      },
+      {
+        path: 'settings',
+        component: SettingsPageComponent,
+      },
+      
+    ],
+  },
+
 ];
 
 @NgModule({
