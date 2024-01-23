32c561b3119686ad29e7a0c4b79b777add87c3e3
Lefan Hu <lefanhu1@gmail.com>
2024-01-22 20:14:18 -0500

    gradient effects for on hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundSize: {
+        'size-200': '200% 200%',
+      },
+      backgroundPosition: {
+        'pos-0': '0% 0%',
+        'pos-100': '100% 100%',
+      },
+    },
   },
   plugins: [],
 }
