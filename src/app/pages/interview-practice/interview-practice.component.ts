db7e66b9df5b38b480f2becf5b00050fd0785688
Derek Li <rejie.li@gmail.com>
2024-03-04 15:19:29 -0500

    reconfigured practice page with /browse data

--- src/app/pages/interview-practice/interview-practice.component.ts
+++ src/app/pages/interview-practice/interview-practice.component.ts
@@ -1,52 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { InterviewSelectionData } from '../interview-select/interview-select.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-interview-practice',
   templateUrl: './interview-practice.component.html',
-  styleUrls: ['./interview-practice.component.css']
+  styleUrls: ['./interview-practice.component.css'],
+})
+@Injectable({
+  providedIn: 'root',
 })
 export class InterviewPracticeComponent implements OnInit {
-  video: any;
-  accessBtn: any;
-  recordBtn: any;
-  stopBtn: any;
-  restartBtn: any;
-  submitBtn: any;
-  countdown: any;
-  countdownNum: any;
-  question: any;
-  time: any;
-  progressBarFill: any;
-  progressBar: any;
-  duration: number = 120;
-  chunks: any[] = [];
-  mediaRecorder: any;
-  countDownInterval: any;
-  questionData: any = {
-    Question: "What is the answer to life?" // Placeholder, replace with actual question data
-  };
+  //TODO create statemachine functionality and use ngIf for the buttons instead of changing its style
+  private video: any;
+  private accessBtn: any;
+  private recordBtn: any;
+  private stopBtn: any;
+  private restartBtn: any;
+  private submitBtn: any;
+  private countdown: any;
+  private countdownNum: any;
+  private time: any;
+  private progressBarFill: any;
+  private progressBar: any;
+  private duration: number = 120;
+  private chunks: any[] = [];
+  private mediaRecorder: any;
+  private countDownInterval: any;
+  data!: InterviewSelectionData;
+  currentTime: number = 0;
 
-  constructor() { }
+  constructor(private router: Router) {
+    const state = this.router.getCurrentNavigation()?.extras.state;
+    if (state) {
+      this.data = state['data'];
+    }
+  }
 
   ngOnInit(): void {
-    this.video = document.getElementById("video");
-    this.accessBtn = document.getElementById("access-camera");
-    this.recordBtn = document.getElementById("record-btn");
-    this.stopBtn = document.getElementById("stop-btn");
-    this.restartBtn = document.getElementById("restart-btn");
-    this.submitBtn = document.getElementById("submit-btn");
-    this.countdown = document.getElementById("countdown");
-    this.countdownNum = document.getElementById("countdown-num");
-    this.question = document.getElementById("question");
-    this.time = document.getElementById("time");
-    this.progressBarFill = document.querySelector(".progress-bar-fill");
-    this.progressBar = document.querySelector(".progress-bar");
+    this.video = document.getElementById('video');
+    this.accessBtn = document.getElementById('access-camera');
+    this.recordBtn = document.getElementById('record-btn');
+    this.stopBtn = document.getElementById('stop-btn');
+    this.restartBtn = document.getElementById('restart-btn');
+    this.submitBtn = document.getElementById('submit-btn');
+    this.countdown = document.getElementById('countdown');
+    this.countdownNum = document.getElementById('countdown-num');
+    this.time = document.getElementById('time');
+    this.progressBarFill = document.querySelector('.progress-bar-fill');
+    this.progressBar = document.querySelector('.progress-bar');
 
     this.accessCamera();
   }
 
   startCountdown() {
-    this.countdown.style.display = "block";
+    this.countdown.style.display = 'block';
     let count = 3;
     this.countdownNum.textContent = count;
     const countdownInterval = setInterval(() => {
@@ -54,22 +62,21 @@ export class InterviewPracticeComponent implements OnInit {
       this.countdownNum.textContent = count;
       if (count === 0) {
         clearInterval(countdownInterval);
-        this.countdown.style.display = "none";
+        this.countdown.style.display = 'none';
         this.mediaRecorder.start();
-        this.stopBtn.style.display = "block";
-  
-        let currentTime = 0;
+        this.stopBtn.style.display = 'block';
+
         this.progressBarFill.style.width = `0%`;
-        this.progressBar.style.display = "block";
+        this.progressBar.style.display = 'block';
         this.countDownInterval = setInterval(() => {
-          currentTime += 1;
-          const progressPercentage = (currentTime / this.duration) * 100;
+          this.currentTime += 1;
+          const progressPercentage = (this.currentTime / this.duration) * 100;
           this.progressBarFill.style.width = `${progressPercentage}%`;
-          var minutes = Math.floor((this.duration - currentTime) / 60);
-          var seconds = (this.duration - currentTime) % 60;
-  
-          this.time.innerHTML = minutes + " m " + seconds + " s";
-          if (currentTime >= this.duration) {
+          let minutes = Math.floor((this.duration - this.currentTime) / 60);
+          let seconds = (this.duration - this.currentTime) % 60;
+
+          this.time.innerHTML = minutes + ' m ' + seconds + ' s';
+          if (this.currentTime >= this.duration) {
             this.mediaRecorder.stop();
             clearInterval(this.countDownInterval);
           }
@@ -85,16 +92,18 @@ export class InterviewPracticeComponent implements OnInit {
         audio: true,
       });
       this.mediaRecorder = new MediaRecorder(this.video.srcObject);
-      this.mediaRecorder.addEventListener("dataavailable", (event: any) => {
+      this.mediaRecorder.addEventListener('dataavailable', (event: any) => {
         this.chunks.push(event.data);
       });
-      this.mediaRecorder.addEventListener("stop", () => {
-        this.stopBtn.style.display = "none";
-        this.restartBtn.style.display = "block";
-        this.submitBtn.style.display = "block";
-        this.progressBarFill.style.width = "100%";
-        let source = document.createElement("source");
-        source.src = URL.createObjectURL(new Blob(this.chunks, { type: "video/mp4" }));
+      this.mediaRecorder.addEventListener('stop', () => {
+        this.stopBtn.style.display = 'none';
+        this.restartBtn.style.display = 'block';
+        this.submitBtn.style.display = 'block';
+        this.progressBarFill.style.width = '100%';
+        let source = document.createElement('source');
+        source.src = URL.createObjectURL(
+          new Blob(this.chunks, { type: 'video/mp4' })
+        );
         clearInterval(this.countDownInterval);
         this.video.appendChild(source);
         this.video.srcObject = null;
@@ -103,10 +112,10 @@ export class InterviewPracticeComponent implements OnInit {
         this.video.controls = true;
         this.video.play();
       });
-      this.recordBtn.style.display = "block";
-      this.accessBtn.style.display = "none";
+      this.recordBtn.style.display = 'block';
+      this.accessBtn.style.display = 'none';
     } catch (error) {
-      alert("An unexpected error has occurred!");
+      alert('An unexpected error has occurred!');
       console.error(error);
     }
   }
@@ -114,9 +123,9 @@ export class InterviewPracticeComponent implements OnInit {
   recordBtnClick() {
     try {
       this.startCountdown();
-      this.recordBtn.style.display = "none";
+      this.recordBtn.style.display = 'none';
     } catch (error) {
-      alert("An unexpected error has occurred!");
+      alert('An unexpected error has occurred!');
       console.error(error);
     }
   }
@@ -125,48 +134,34 @@ export class InterviewPracticeComponent implements OnInit {
     try {
       this.mediaRecorder.stop();
     } catch (error) {
-      alert("An unexpected error has occurred!");
+      alert('An unexpected error has occurred!');
       console.error(error);
     }
   }
 
   submitBtnClick() {
-    try {
-      const blob = new Blob(this.chunks, { type: "video/mp4" });
-
-      const formData = new FormData();
-      formData.append("questionId", this.questionData.questionId);
-      formData.append("video", blob, "video.mp4");
-      // Replace the URL with your actual endpoint
-      fetch("/api/processInterview", {
-        method: "POST",
-        body: formData
-      })
-      .then(response => response.json())
-      .then(data => {
-        // Handle the server response
-      })
-      .catch(error => {
-        // Handle any errors
-        console.error(error);
-      });
-      this.chunks = [];
-    } catch (error) {
-      alert("An unexpected error has occurred!");
-      console.error(error);
-    }
+    //TODO(alex) submit for analyzing
+    const blob = new Blob(this.chunks, { type: 'video/mp4' });
   }
 
   restartBtnClick() {
     try {
       location.reload();
     } catch (error) {
-      alert("An unexpected error has occurred!");
+      alert('An unexpected error has occurred!');
       console.error(error);
     }
   }
 
   ngOnDestroy() {
     window.onbeforeunload = null;
+  }
+
+  setData(data: any) {
+    this.data = data;
+  }
+
+  getData() {
+    return this.data;
   }
 }
